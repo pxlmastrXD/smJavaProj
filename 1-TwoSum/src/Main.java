fdb5269b37cbb113d5c087fbaceab6dcac408c00
Pxlmastr <44294279+pxlmastrXD@users.noreply.github.com>
2025-03-07 07:39:03 -0500

    💾 Added primary structure for project #1 (Two Sum)

---
+++ 1-TwoSum/src/Main.java
@@ -0,0 +1,14 @@
+/*
+Project 1: TWO SUM
+Objective: Complete the two sum problem using Java.
+
+Made by @pxlmastrXD
+
+*/
+
+// Main function: Tests the TwoSum classfile
+class Main {
+    public static void main(String[] args) {
+        
+    }
+}
