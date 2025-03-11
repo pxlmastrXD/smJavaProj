a23c5c41e2af8807ca542a2cef716f394eaaed5f
pxlmastr <44294279+pxlmastrXD@users.noreply.github.com>
2025-03-11 11:34:28 -0400

    💾 Added primary structure for CalqJava (Project #2)

---
+++ 2-CalqJava/src/Main.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args) {
+        var num = 69;
+        System.out.println(num);
+    }
+}
---
+++ 2-CalqJava/src/Math.java
@@ -0,0 +1,16 @@
+class Math {
+    var num1 = Integer(0);
+    var num2 = Integer(0);
+
+    Math(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+
+    // TODO: #1 Add Operations for Math class
+    int add() {}
+    int sub() {}
+    int mul() {}
+    int div() {}
+}
