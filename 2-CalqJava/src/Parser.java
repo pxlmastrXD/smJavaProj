cc1323bd6b05dcb68c46c5406ea78b567d332ce8
pxlmastr <44294279+pxlmastrXD@users.noreply.github.com>
2025-03-12 07:56:34 -0400

    💾 Added a bit of the Parser class, will finish tomorrow.

--- 2-CalqJava/src/Parser.java
+++ 2-CalqJava/src/Parser.java
@@ -1,1 +1,24 @@
-// TODO: #2 Add Argument parsing
+class Parser {
+    String[] args = ["default"];
+    public int num1 = 0;
+    public int operation = Operations.ADD;
+    public int num2 = 0
+
+    public enum Operations {
+        ADD,
+        SUB,
+        MUL,
+        DIV
+    }
+
+    Parser(String[] args) {
+        this.args = args;
+    }
+
+    public void parse() {
+        this.num1 = (int) args[1];
+        var tmpOp = args[2];
+        this.num2 = (int) args[3];
+        // 
+    }
+}
