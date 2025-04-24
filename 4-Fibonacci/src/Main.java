3d2c41496e0f9415167985201c72d963e64b09da
pxlmastr <calebwietholter9@outlook.com>
2025-04-24 12:57:27 +0000

    ⭐ Finished Fibonacci project.

--- 4-Fibonacci/src/Main.java
+++ 4-Fibonacci/src/Main.java
@@ -1,5 +1,14 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println(Fib.fib(5));
+        if (args.length == 0) {
+            System.out.println("""
+            No arguments provided. 
+            Usage: java -jar fibonacci.jar {number}
+            """);
+            System.exit(1);
+        };
+        var argument = args[0];
+        var number = Integer.parseInt(argument);
+        System.out.println(Fib.fib(number));
     }
 }
